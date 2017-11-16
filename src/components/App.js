038c2eff6d7ffa9a41ccd17535669eb24d35bfa0
Peter <peter.prentiss@gmail.com>
2017-11-16 10:06:59 -0600

    Basic project setup.

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import SearchBar from '../containers/SearchBar';
+
+export default class App extends Component {
+  render() {
+    return (
+      <SearchBar />
+    );
+  }
+}
