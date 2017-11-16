a395ee9198b101a7b4d97521eaf1894a279af794
Peter <peter.prentiss@gmail.com>
2017-11-16 16:11:48 -0600

    Set up API call and keyconfig for openweather

---
+++ src/actions/index.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+import OpenWeatherKey from '../modules/keyConfig';
+
+const API_KEY = process.env.WEATHER_API_KEY || OpenWeatherKey.WEATHER_API_KEY;
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?id=524901&APPID=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+    const url = `${ROOT_URL}&q=${city},us`;
+    const request = axios.get(url);
+
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    };
+}
