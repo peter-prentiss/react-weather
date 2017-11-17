5a8272a6387e29c8e437c9d7493d523f209a7846
Peter <peter.prentiss@gmail.com>
2017-11-16 19:41:13 -0600

    View temperature, pressure, and humidity for each city.

--- src/containers/WeatherList.js
+++ src/containers/WeatherList.js
@@ -1,12 +1,31 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import {Sparklines, SparklinesLine} from 'react-sparklines';
 
 class WeatherList extends Component {
     renderWeather(cityData) {
+        const temps = cityData.list.map(weather => weather.main.temp);
+        const pressures = cityData.list.map(weather => weather.main.pressure);
+        const humidities = cityData.list.map(weather => weather.main.humidity);
+
         return (
             <tr key={cityData.city.id}>
                 <td>{cityData.city.name}</td>
-                <td>{cityData.list[0]}</td>
+                <td>
+                    <Sparklines height={120} width={180} data={temps}>
+                        <SparklinesLine color="red" />
+                    </Sparklines>
+                </td>
+                <td>
+                    <Sparklines height={120} width={180} data={pressures}>
+                        <SparklinesLine color="yellow" />
+                    </Sparklines>
+                </td>
+                <td>
+                    <Sparklines height={120} width={180} data={humidities}>
+                        <SparklinesLine color="blue" />
+                    </Sparklines>
+                </td>
             </tr>
         )
     }
